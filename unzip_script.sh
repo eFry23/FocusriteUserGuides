dceb8249a4137dc420b28ccb8ac1d33d0ae99c26
eFry23 <154227682+eFry23@users.noreply.github.com>
2023-12-18 14:20:58 +0000

    Create unzip_script.sh

---
+++ unzip_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set the file name
+zip_file="RedNet_PCIeNX-html5_1702908970.1115.zip"
+
+# Unzip the file
+unzip $zip_file -d main
+
+# Optionally, you can remove the original zip file if needed
+# rm $zip_file
