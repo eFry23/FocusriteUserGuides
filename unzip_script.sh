309805071519ee1f3f98d4d6b90d26a4b987b15f
eFry23 <154227682+eFry23@users.noreply.github.com>
2023-12-18 14:23:51 +0000

    Update unzip_script.sh

--- unzip_script.sh
+++ unzip_script.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Set the file name
-zip_file="RedNet_PCIeNX-html5_1702908970.1115.zip"
+# Find the zip file in the current directory
+zip_file=$(find . -maxdepth 1 -type f -name '*.zip' -print -quit)
 
-# Unzip the file
-unzip $zip_file -d main
+# Check if a zip file was found
+if [ -n "$zip_file" ]; then
+  # Unzip the file
+  unzip "$zip_file" -d main
 
-# Optionally, you can remove the original zip file if needed
-# rm $zip_file
+  # Optionally, you can remove the original zip file if needed
+  # rm "$zip_file"
+else
+  echo "No zip file found in the current directory."
+fi
+
