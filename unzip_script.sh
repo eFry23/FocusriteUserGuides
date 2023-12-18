e1a5d98130d2f58f5e605d0deb84cb2f5aca3f1e
eFry23 <154227682+eFry23@users.noreply.github.com>
2023-12-18 14:29:08 +0000

    Update unzip_script.sh

--- unzip_script.sh
+++ unzip_script.sh
@@ -5,12 +5,18 @@ zip_file=$(find . -maxdepth 1 -type f -name '*.zip' -print -quit)
 
 # Check if a zip file was found
 if [ -n "$zip_file" ]; then
+  echo "Found zip file: $zip_file"
+
+  # Print the contents of the zip file (for debugging purposes)
+  unzip -l "$zip_file"
+
   # Unzip the file
-  unzip "$zip_file" -d FocusriteUserGuides
+  unzip -q "$zip_file" -d FocusriteUserGuides
+
+  echo "Unzipped successfully."
 
   # Optionally, you can remove the original zip file if needed
   # rm "$zip_file"
 else
   echo "No zip file found in the current directory."
 fi
-
